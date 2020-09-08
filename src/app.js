f774671393816845025085f38ef391817692c103
AnduMekon <anduabe@yahoo.com>
2020-09-08 16:56:04 -0400

    3rd

--- src/app.js
+++ src/app.js
@@ -1,25 +1,5 @@
-// install -> import -> use
-import React from "react";
-import ReactDOM from "react-dom";
-class Template extends React.Component {
-  render() {
-    return (
-      <div style={{ background: "black", display: "block", margin: "0" }}>
-        <h1 style={{ color: "red", textAlign: "center" }}> This is greate</h1>
-        <h3 style={{ textAlign: "center", color: "green" }}>xxxxxx</h3>
-        <div>
-          <h2 style={{ color: "yellow", textAlign: "center" }}>xxxxxxx</h2>
-        </div>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-        <div>
-          <form style={{ background: "yellow" }}>
-            Name= <input />
-            Age= <input />
-          </form>
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Template />, document.getElementById("app"));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
---
+++ src/playground/ck.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+class Andu extends React.Component {
+  render() {
+    return <div>test</div>;
+  }
+}
+ReactDOM.render(<Andu />, document.getElementById("app"));
