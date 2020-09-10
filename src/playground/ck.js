444dc371abb1a7b16230c512a2b766dda5ec027b
AnduMekon <anduabe@yahoo.com>
2020-09-09 23:31:49 -0400

    4th

--- src/playground/ck.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-class Andu extends React.Component {
-  render() {
-    return <div>test</div>;
-  }
-}
-ReactDOM.render(<Andu />, document.getElementById("app"));
